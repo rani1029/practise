58e0715fbf9a3665571944b88325dad726b83eb5
rani1029 <ranig1029@gmail.com>
2021-08-01 23:52:12 +0530

    added file visual

---
+++ practisevisual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practisevisual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
